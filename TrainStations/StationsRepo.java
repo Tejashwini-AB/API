814f5a6d42171a0e982a237b9b47212fc2d8ac19
Tejashwini A B <117555919+Tejashwini-AB@users.noreply.github.com>
2024-03-26 07:45:07 +0530

    Update StationsRepo.java
    Added queries to get location by ID and location by Name

--- TrainStations/StationsRepo.java
+++ TrainStations/StationsRepo.java
@@ -1,7 +1,16 @@
 package pkg1.trainstations2;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StationsRepo extends JpaRepository<StationsEntity,Integer> {
+	@Query(value="select station_name,station_code from stations where id=:id",nativeQuery=true)
+	public List<String> getLoactionByID(@Param("id")int id);
+	@Query(value="select station_code,station_name from stations where station_name=:station_name",nativeQuery=true)
+	public List<String> getLocationByName(@Param("station_name")String station_name);
+	
 
 }
+
